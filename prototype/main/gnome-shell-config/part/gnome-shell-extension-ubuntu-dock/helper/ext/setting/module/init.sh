977081299cf0bfee5bb0a63a4f3b925d6e3a1753
samwhelp <samwhelp@users.noreply.github.com>
2024-04-13 20:49:20 +0800

    fix

---
+++ prototype/main/gnome-shell-config/part/gnome-shell-extension-ubuntu-dock/helper/ext/setting/module/init.sh
@@ -0,0 +1,24 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Extension / Ubuntu Dock
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-extension-ubuntu-dock.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
