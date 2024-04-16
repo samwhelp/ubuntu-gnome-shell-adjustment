ae3e116428c040cbe8eae03cbb16c1d81c978c14
samwhelp <samwhelp@users.noreply.github.com>
2024-04-16 22:30:44 +0800

    fix

--- prototype/main/tool-config/part/gnome-text-editor/helper/ext/setting/module/gnome-text-editor.sh
+++ prototype/main/tool-config/part/gnome-text-editor/helper/ext/setting/module/gnome-text-editor.sh
@@ -145,132 +145,132 @@ gnome_text_editor_settings_set_all () {
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor auto-indent true"
 	gsettings set org.gnome.TextEditor auto-indent true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor auto-save-delay 3"
 	gsettings set org.gnome.TextEditor auto-save-delay 3
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor custom-font 'Monospace 14'"
 	gsettings set org.gnome.TextEditor custom-font 'Monospace 14'
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor discover-settings true"
 	gsettings set org.gnome.TextEditor discover-settings true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor draw-spaces \"[]\""
 	gsettings set org.gnome.TextEditor draw-spaces "[]"
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor enable-snippets false"
 	gsettings set org.gnome.TextEditor enable-snippets false
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor highlight-current-line true"
 	gsettings set org.gnome.TextEditor highlight-current-line true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor highlight-matching-brackets true"
 	gsettings set org.gnome.TextEditor highlight-matching-brackets true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor indent-style 'tab'"
 	gsettings set org.gnome.TextEditor indent-style 'tab'
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor indent-width -1"
 	gsettings set org.gnome.TextEditor indent-width -1
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor keybindings 'default'"
 	gsettings set org.gnome.TextEditor keybindings 'default'
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor last-save-directory ''"
 	gsettings set org.gnome.TextEditor last-save-directory ''
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor line-height 1.2"
 	gsettings set org.gnome.TextEditor line-height 1.2
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor recolor-window true"
 	gsettings set org.gnome.TextEditor recolor-window true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor restore-session false"
 	gsettings set org.gnome.TextEditor restore-session false
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor right-margin-position 80"
 	gsettings set org.gnome.TextEditor right-margin-position 80
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor show-grid false"
 	gsettings set org.gnome.TextEditor show-grid false
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor show-line-numbers true"
 	gsettings set org.gnome.TextEditor show-line-numbers true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor show-map true"
 	gsettings set org.gnome.TextEditor show-map true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor show-right-margin true"
 	gsettings set org.gnome.TextEditor show-right-margin true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor spellcheck true"
 	gsettings set org.gnome.TextEditor spellcheck true
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor style-scheme 'solarized-light'"
 	gsettings set org.gnome.TextEditor style-scheme 'solarized-light'
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor style-variant 'follow'"
 	gsettings set org.gnome.TextEditor style-variant 'follow'
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor tab-width 8"
 	gsettings set org.gnome.TextEditor tab-width 8
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor use-system-font false"
 	gsettings set org.gnome.TextEditor use-system-font false
 
 
 	util_error_echo
-	util_error_echo ""
+	util_error_echo "gsettings set org.gnome.TextEditor wrap-text true"
 	gsettings set org.gnome.TextEditor wrap-text true
 
 
