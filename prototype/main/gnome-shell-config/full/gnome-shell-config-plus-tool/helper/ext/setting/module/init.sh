8494e4a43f7da4e8978a037b84252655fc71a2af
samwhelp <samwhelp@users.noreply.github.com>
2024-04-16 22:37:39 +0800

    fix

--- prototype/main/gnome-shell-config/full/gnome-shell-config-plus-tool/helper/ext/setting/module/init.sh
+++ prototype/main/gnome-shell-config/full/gnome-shell-config-plus-tool/helper/ext/setting/module/init.sh
@@ -62,5 +62,12 @@
 
 
 ##
+## Load: Setting / Module / Nautilus
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/nautilus.sh"
+
+
+##
 ### Tail: Setting / Module / Init
 ################################################################################
