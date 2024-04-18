bfd43b412df12c1212946f57d9e8d67db92e93cd
samwhelp <samwhelp@users.noreply.github.com>
2024-04-18 18:40:45 +0800

    fix

---
+++ prototype/main/gnome-shell-config/part/gnome-shell-theme-dark/helper/ext/setting/module/init.sh
@@ -0,0 +1,24 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Theme Light
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-theme-light.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
