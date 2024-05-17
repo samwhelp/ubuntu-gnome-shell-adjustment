7df178fcaebaac674e55e752342eb1e79a895a22
samwhelp <samwhelp@users.noreply.github.com>
2024-05-17 18:41:51 +0800

    fix

--- prototype/main/gnome-shell-config/full/gnome-shell-config-plus-keybind/prototype-install.sh
+++ prototype/main/gnome-shell-config/full/gnome-shell-config-plus-keybind/prototype-install.sh
@@ -43,6 +43,8 @@ main_prototype_install () {
 
 	gnome_shell_workspace_main_prototype_install
 
+	gnome_shell_favorite_apps_prototype_install
+
 	gnome_shell_extension_dash_to_dock_prototype_install
 
 	gnome_shell_keybind_main_prototype_install
