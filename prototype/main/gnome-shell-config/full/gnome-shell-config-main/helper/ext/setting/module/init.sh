c52e18e0fce65d910676a6845ea5216ea151f4f3
samwhelp <samwhelp@users.noreply.github.com>
2024-05-17 18:29:32 +0800

    fix

--- prototype/main/gnome-shell-config/full/gnome-shell-config-main/helper/ext/setting/module/init.sh
+++ prototype/main/gnome-shell-config/full/gnome-shell-config-main/helper/ext/setting/module/init.sh
@@ -27,6 +27,13 @@
 
 
 ##
+## Load: Setting / Module / Gnome Shell / Favorite Apps
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-favorite-apps.sh"
+
+
+##
 ## Load: Setting / Module / Gnome Shell / Extension / Dash To Dock
 ##
 
